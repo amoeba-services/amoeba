fdbf84698b90dde6f3da53f9d0efc7e729e08d57
leeyeh <lee55962698@gmail.com>
2014-10-28 18:28:34 +0800

    seperate routers from controllers for better readability.

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -2,6 +2,7 @@ var express = require('express'),
   router = express.Router();
 
 require('../routers/api')(router);
+require('../routers/revision')(router);
 
 module.exports = function (app) {
   app.use('/apis', router);
---
+++ app/routers/data.js
@@ -0,0 +1,92 @@
+var _ = require('underscore'),
+  mongoose = require('mongoose'),
+  Api = mongoose.model('Api'),
+  util = {
+    uri: require('../utils/uri-parser')
+  };
+
+var CONTENT_TYPES = {
+  'json': 'application/json',
+  'text': 'text/plain'
+};
+
+module.exports = function (router) {
+  router.route('/:namespace/:uri')
+  .all(function requstInfoParser(req, res, next) {
+      var target = req.target = util.uri.parse(req.params.uri);
+      var api = req.api = {
+        namespace: req.params.namespace,
+        path: target.path
+      };
+      try {
+        api = Api.normalizeKeys(api);
+      }
+      catch (err) {
+        err.code = 4000;
+        next(err);
+      }
+
+      var info = req.info = {};
+      _(['method', 'headers', 'body']).forEach(function(keyName) {
+        info[keyName] = req[keyName];
+      });
+      _(['scheme', 'host', 'port', 'params']).forEach(function(keyName) {
+        info[keyName] = target[keyName];
+      });
+      next();
+  })
+  .all(function apiMatcher(req, res, next) {
+    Api.findOne(req.api, function (err, api) {
+      if (err) return next(err);
+      if (api === null) {
+        //没有匹配的 api
+        err = new Error('no API matched');
+        err.code = 4004;
+        err.status = 404;
+        return next(err);
+      }
+      res.api = api;
+      next();
+    });
+  })
+  .all(function apiInfoHeadersSetter(req, res, next) {
+    var api = res.api;
+    res.set('X-Amoeba-Namespace', api.namespace);
+    res.set('X-Amoeba-Matched-API', api.path);
+    next();
+  })
+  .all(function disabledApiChecker(req, res, next) {
+    if (res.api.disabled) {
+      var err = new Error('API disabled');
+      err.code = 4003;
+      err.status = 404;
+      return next(err);
+    }
+    next();
+  })
+  .all(function router(req, res, next) {
+    var api = res.api;
+    res.info = api.route[0].response.content;
+    if (res.info === undefined) {
+      var err = new Error('no rule matched');
+      err.code = 4002;
+      err.status = 404;
+      return next(err);
+    }
+    next();
+  })
+  .all(function echoResponse(req, res, next) {
+    var info = res.info;
+    if (info.headers['Content-Type'] === undefined) {
+      info.headers['Content-Type'] = CONTENT_TYPES[info.type];
+    }
+    if (info.type === 'json') {
+      info.body = JSON.stringify(info.body);
+    }
+    res.status(info.status || 200);
+    _(info.headers).forEach(function(value, key) {
+      res.set(key, value);
+    });
+    res.send(info.body);
+  });
+};
