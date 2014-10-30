c0e1bd45e266ebdebc1c090047dcdad35fde6ad7
leeyeh <lee55962698@gmail.com>
2014-10-30 19:47:29 +0800

    revisions -> revision.

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -2,7 +2,7 @@ var express = require('express'),
   router = express.Router();
 
 require('../routers/api')(router);
-require('../routers/revisions')(router);
+require('../routers/revision')(router);
 
 module.exports = function (app) {
   app.use('/apis', router);
