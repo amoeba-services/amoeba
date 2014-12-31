amoeba-services/amoeba

commit 9f5754690cf27331efad28401eb572a767042571

`response.headers` is now an array (instead of an object).

app/routers/data.js
@@ -92,8 +92,8 @@ module.exports = function (router) {
       info.body = JSON.stringify(info.body);
     }
     res.status(info.status || 200);
-    _(info.headers).forEach(function(value, key) {
-      res.set(key, value);
+    _(info.headers).forEach(function(header) {
+      res.set(header.key, header.value);
     });
     res.set('X-Amoeba-Status', 2000);
     res.set('X-Amoeba-Message', 'OK');

commit 5c30b1b51f7a804c8ef6bb69cc28bf1ce696d6af

Fix the `Content-Type` not working bug caused by 9f5754690cf27331efad28401eb572a767042571.

app/routers/data.js
@@ -82,12 +82,13 @@ module.exports = function (router) {
   .all(util.analytics.send())
   .all(function echoResponse(req, res, next) {
     var info = res.info;
-    if (info.headers === undefined) {
-      info.headers = {};
+    if (!_.isArray(info.headers)) {
+      info.headers = [];
     }
-    if (info.headers['Content-Type'] === undefined) {
-      info.headers['Content-Type'] = CONTENT_TYPES[info.type || DEFAULT_CONTENT_TYPE];
-    }
+    info.headers.push({
+      key: 'Content-Type',
+      value: CONTENT_TYPES[info.type || DEFAULT_CONTENT_TYPE]
+    });
     if (info.type === 'json') {
       info.body = JSON.stringify(info.body);
     }